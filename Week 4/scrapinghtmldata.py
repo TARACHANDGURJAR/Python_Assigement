5c6039a3c0c95a070e30e58a8b6db2872d203989
Taracahnd Gurjar <201451067@iiitvadodara.ac.in>
2018-01-21 23:57:31 +0530

    Add files via upload

---
+++ Week 4/scrapinghtmldata.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+import math
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+
+# html.parser is the HTML parser included in the standard Python 3 library.
+# information on other HTML parsers is here:
+# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+sum = 0
+for tag in tags:
+    num = int(tag.contents[0])
+    sum = sum + num
+print(sum)
